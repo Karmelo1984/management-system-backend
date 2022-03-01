596499f6e316ed836a26d1d549b405c0e6e23fab
Karmelo1984 <79457759+Karmelo1984@users.noreply.github.com>
2022-03-01 19:41:49 +0100

    Primeros pasos con nestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskController } from './task/task.controller';
+
+/**
+ * Cada módulo contiene una parte de la aplicación.
+ * Cada módulo tiene distinta funcionalidad.
+ * Una aplicación puede estar formada de varios módulos.
+ */
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, TaskController],
   providers: [AppService],
 })
 export class AppModule {}
